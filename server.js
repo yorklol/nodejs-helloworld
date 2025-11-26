6ff36f980a581271c0c846f933a52b0cb25bf15f
斓溪 <york.yy@alibaba-inc.com>
2025-11-26 14:19:45 +0800

    fix bug

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   // 设置响应头
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
   // 返回 Hello World 响应
   res.end('Hello, World!\n');
 });
@@ -13,5 +12,6 @@ const server = http.createServer((req, res) => {
 // 监听端口
 const PORT = 3000;
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running at http://localhost:${PORT}/`);
 });
